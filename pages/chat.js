d075890fc1a0b292cd26248060ea250764dea05d
VyctorCosta <vyctorcosta00@gmail.com>
2022-02-02 21:54:38 -0300

    feat: Criaçao do chat com mensagens, icone do usuario e botao de enviar

--- pages/chat.js
+++ pages/chat.js
@@ -1,31 +1,214 @@
-import { useRouter } from "next/router";
-import { Box, Button } from "@skynexui/components";
+import React from "react";
+import { Box, Text, TextField, Image, Button, Link } from "@skynexui/components";
 import appConfig from "../config.json";
 
 function Chat() {
-    const route = useRouter();
-    return (
-        <>
-            <h1>Bem vindo(a). <br /> Infelizmente essa pagina está incompleta no momento. <br /> Por favor volte mais tarde. <br />
-            <Button
-                label="Voltar para página anterior"
+  const [arrayMessage, setArrayMessage] = React.useState([]);
+  const [message, setMessage] = React.useState("");
+
+  function handleNewMessage(newMessage) {
+    setArrayMessage([
+        {
+          id: arrayMessage.length + 1,
+          from: "VyctorCosta",
+          message: newMessage,
+        },
+      ...arrayMessage
+    ]);
+    setMessage("");
+  }
+
+  return (
+    <Box
+      styleSheet={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: appConfig.theme.colors.primary[500],
+        backgroundImage: `url(/img/background.png)`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundBlendMode: "multiply",
+        color: appConfig.theme.colors.neutrals["000"],
+      }}
+    >
+      <Box
+        styleSheet={{
+          display: "flex",
+          flexDirection: "column",
+          flex: 1,
+          boxShadow: "0 2px 10px 0 rgb(0 0 0 / 20%)",
+          borderRadius: "5px",
+          backgroundColor: appConfig.theme.colors.neutrals[700],
+          height: "100%",
+          maxWidth: "95%",
+          maxHeight: "95vh",
+          padding: "32px",
+        }}
+      >
+        <Header />
+        <Box
+          styleSheet={{
+            position: "relative",
+            display: "flex",
+            flex: 1,
+            height: "80%",
+            backgroundColor: appConfig.theme.colors.neutrals[600],
+            flexDirection: "column",
+            borderRadius: "5px",
+            padding: "16px",
+          }}
+        >
+          <MessageList arrayMessage={arrayMessage} />
+
+          <Box
+            as="form"
+            styleSheet={{
+              display: "flex",
+              alignItems: "center",
+              gap: "25px"
+            }}
+          >
+            <a href={`https://github.com/${"VyctorCosta"}`} target="_blank">
+                <Image
                 styleSheet={{
-                    marginTop: "40px"
+                    width: "55px",
+                    height: "55px",
+                    borderRadius: "50%",
+                    display: "inline-block",
+                    marginRight: "4px",
                 }}
-                onClick={event => {
-                    route.push("/")
-                }}
-                buttonColors={{
-                    contrastColor: appConfig.theme.colors.neutrals["000"],
-                    mainColor: appConfig.theme.colors.neutrals[300],
-                    mainColorLight: appConfig.theme.colors.neutrals[500],
-                    mainColorStrong: appConfig.theme.colors.neutrals[600],
-                }}
+                src={`https://github.com/${"VyctorCosta"}.png`}
                 />
-            </h1>
-            
-        </>
-    );
+            </a>
+            <TextField
+              value={message}
+              placeholder="Insira sua mensagem aqui..."
+              type="textarea"
+              onChange={(e) => {
+                setMessage(e.target.value);
+              }}
+              onKeyPress={(e) => {
+                if (e.key === "Enter" && !e.shiftKey) {
+                  e.preventDefault();
+                  handleNewMessage(message);
+                }
+              }}
+              styleSheet={{
+                width: "100%",
+                border: "0",
+                resize: "none",
+                borderRadius: "5px",
+                padding: "6px 8px",
+                backgroundColor: appConfig.theme.colors.neutrals[800],
+                marginRight: "12px",
+                color: appConfig.theme.colors.neutrals[200],
+              }}
+            />
+            <Button 
+                label="Enviar"
+                variant="tertiary"
+                colorVariant="neutral"
+                onClick={() => {
+                    handleNewMessage(message);
+                }}
+                styleSheet={{
+                    width: "80px",
+                    height: "45px",
+                    backgroundColor: appConfig.theme.colors.neutrals[800]
+                }}
+            />
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
 }
 
-export default Chat
+function Header() {
+  return (
+    <>
+      <Box
+        styleSheet={{
+          width: "100%",
+          marginBottom: "16px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Text variant="heading5">Chat</Text>
+        <Button
+          variant="tertiary"
+          colorVariant="neutral"
+          label="Logout"
+          href="/"
+        />
+      </Box>
+    </>
+  );
+}
+
+function MessageList({ arrayMessage }) {
+  return (
+    <Box
+      tag="ul"
+      styleSheet={{
+        overflow: "scroll",
+        display: "flex",
+        flexDirection: "column-reverse",
+        flex: 1,
+        color: appConfig.theme.colors.neutrals["000"],
+        marginBottom: "16px",
+      }}
+    >
+      {arrayMessage.map((objMessage) => {
+        return (
+          <Text
+            tag="li"
+            key={objMessage.id}
+            styleSheet={{
+              borderRadius: "5px",
+              padding: "6px",
+              marginBottom: "12px",
+              hover: {
+                backgroundColor: appConfig.theme.colors.neutrals[700],
+              },
+            }}
+          >
+            <Box
+              styleSheet={{
+                marginBottom: "8px",
+              }}
+            >
+              <Image
+                styleSheet={{
+                  width: "20px",
+                  height: "20px",
+                  borderRadius: "50%",
+                  display: "inline-block",
+                  marginRight: "8px",
+                }}
+                src={`https://github.com/${objMessage.from}.png`}
+              />
+              <Text tag="strong">{objMessage.from}</Text>
+              <Text
+                styleSheet={{
+                  fontSize: "10px",
+                  marginLeft: "8px",
+                  color: appConfig.theme.colors.neutrals[300],
+                }}
+                tag="span"
+              >
+                {new Date().toLocaleDateString("pt-BR", {hour: "numeric", minute: "numeric"})}
+              </Text>
+            </Box>
+            {objMessage.message}
+          </Text>
+        );
+      })}
+    </Box>
+  );
+}
+
+export default Chat;
